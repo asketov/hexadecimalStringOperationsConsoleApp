1aa5eb9df794c806e98b77d428a5da41cfdda696
asketov <80895930+asketov@users.noreply.github.com>
2021-04-04 16:43:26 +0300

    Update 1 pr.cpp

--- 1 pr.cpp
+++ 1 pr.cpp
@@ -12,7 +12,7 @@ using namespace std;
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    char menu;
+    int menu;
     triangle Triangle;
     cout << "Введите длину стороны треугольника:";
     Triangle.set_side1(Triangle.write_side1());
@@ -20,6 +20,7 @@ int main()
     Triangle.set_angle1(Triangle.write_angle1());
     cout << "Введите величину второго угла:";
     Triangle.set_angle2(Triangle.write_angle2());
+    getchar();
     do {
         system("cls");
         cout << "1. Периметр треугольника" << endl;
@@ -29,24 +30,31 @@ int main()
         cout << "5. Напечатать треугольник" << endl;
         cout << "6. Создать треугольник с помощью конструктора" << endl;
         cout << "7. Проверить конструктор копирования" << endl;
-        cout << "8. Выход" << endl;
+        cout << "8. Установить новое значение углу 1" << endl;
+        cout << "9. Установить новое значение стороне" << endl;
+        cout << "10. Установить новое значение углу 2" << endl;
+        cout << "11. Вывести значение 1 угла" << endl;
+        cout << "12. Вывести значение стороны" << endl;
+        cout << "13. Вывести значение 2 угла" << endl;
+        cout << "14. Выход" << endl;
         cin >> menu;
+        
         switch (menu)
         {
-            case '1':
+            case 1:
             {  system("cls");
                cout << "Периметр равен:" << Triangle.perimetr();
-               getchar();
+               getchar(); 
                break;
             }
-            case '2':
+            case 2:
             { 
                system("cls");
                cout << "Медиана равна:" << Triangle.mediana();
                getchar();
                break;
             }
-            case '3':
+            case 3:
             {
                 system("cls");
                 cout << "Радиус равен:" << Triangle.radius();
@@ -54,7 +62,7 @@ int main()
                 break;
             }
 
-            case '4':
+            case 4:
             { 
                triangle Triangle2,Triangle3;
                system("cls");
@@ -83,14 +91,14 @@ int main()
                getchar();
                break;
             }
-            case '5':
+            case 5:
             {
                 system("cls");
                 Triangle.print_triangle();
                 getchar();
                 break;
             }
-            case '6':
+            case 6:
             {
                 system("cls");
                 double side1, angle1, angle2;
@@ -105,7 +113,7 @@ int main()
                 getchar();
                 break;
             }
-            case '7':
+            case 7:
             {
                 system("cls");
                 double side1, angle1, angle2;
@@ -120,8 +128,50 @@ int main()
                 getchar();
                 break;
             }
+            case 8:
+            {
+                system("cls");
+                cout << "Введите новое значение 1 угла:";
+                Triangle.set_angle1(Triangle.write_angle1());
+                break;
+            }
+            case 9:
+            {
+                system("cls");
+                cout << "Введите новое значение стороны:";
+                Triangle.set_side1(Triangle.write_side1());
+                break;
+            }
+            case 10:
+            {
+                system("cls");
+                cout << "Введите новое значение 2 угла:";
+                Triangle.set_angle2(Triangle.write_angle2());
+                break;
+            }
+            case 11:
+            {
+                system("cls");
+                cout << "Значение 1 угла равно:" << Triangle.get_angle1();
+                getchar();
+                break;
+            }
+            case 12:
+            {
+                system("cls");
+                cout << "Значение стороны равно:" << Triangle.get_side1();
+                getchar();
+                break;
+            }
+            case 13:
+            {
+                system("cls");
+                cout << "Значение 2 угла равно:" << Triangle.get_angle2();
+                getchar();
+                break;
+            }
         }
         getchar();
-    } while (menu != '8');
+    } while (menu != 14);
 }
 
